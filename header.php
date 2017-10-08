197b6518d077e8ea6eda9a7b512ead6711b753f1
Yaroslav Taranenko <yarikT94@gmail.com>
2017-10-08 18:33:46 +0300

    added core styles minified

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 
     <!-- loftloader styles -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/style.min.css" type="text/css" media="screen">
     <link rel="icon" href="<?php bloginfo('template_url')?>/favicons/favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_url')?>/favicons/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_url')?>/favicons/apple-icon-60x60.png">
